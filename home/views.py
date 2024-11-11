651a23b5c77ea64c0aaf4967b106cd4065149837
Adoroteo01 <andreitot13@gmail.com>
2024-11-10 16:12:49 -0800

    connected images in home

--- home/views.py
+++ home/views.py
@@ -1,6 +1,8 @@
 from django.shortcuts import render
-
+from moodchooser.models import MoodEntry
 # Create your views here.
 
 def home(response):
-    return render(response, "home.html")
+
+    all_mood_entries = MoodEntry.objects.all
+    return render(response, "home.html", {"moods":all_mood_entries})
